18c57856a660b00985153d05d47e1654c0bfe795
fitsum <fitsumbelay@gmail.com>
2019-11-13 07:56:30 -0500

    collection of quits and actions + removed container events and actions

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
 var blessed = require('blessed');
 
-var screen = blessed.screen({smartCSR: true});
+var screen = blessed.screen({smartCSR: false});
 screen.title = "Blessed Sketch 001";
 
 const boxContent = ['one','two','three'];
 
-let outerBox = blessed.box({
-	top: 'center',
-	left: 'center',
-	width: '50%',
-	height: '50%',
-	content: boxContent[1],
+let container = blessed.box({
+	top: 'top',
+	right: '1',
+	width: '300',
+	height: '400',
+	content: 'Cholly!',
 	style: {
-		fg: '#006',
+		fg: 'red',
 		bg: 'green',	
 	}
 });
 
-let innerBox = blessed.box({
-	top: 'center',
-	left: 'center',
-	width: '25%',
-	height: '25%',
-	style: {bg: 'white'}
-})
+container.key('enter', (ch, key)=>{
+	container.setContent('Centered content FFS');
+	screen.render();
+});
 
 
-screen.key(['q','escape','C-c'],(ch, key)=>{
+let quits = ['q', 'escape','C-c'];
+
+let actions = {
+	'up': 'up or forward',
+	'right': 'right',
+	'down': 'down or back',
+	'left': 'left',
+	'space': 'spacebar!',
+}
+
+
+screen.key(quits, (ch, key)=>{
 	return process.exit(0);
-})
+});
 
+screen.key(Object.keys(actions), (ch, key)=>{
+	container.setContent(`action: ${actions[key.name]}`);
+	screen.render();
+});
 
-screen.append(outerBox);
-screen.append(innerBox);
-
-outerBox.focus();
-
+screen.append(container);
 screen.render();
 	
